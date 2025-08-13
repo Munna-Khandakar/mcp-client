c4a133567b70f06edcdc1cbad9634c0ca353c613
Khan, Abdullah <abdullah.khan@ideascale.com>
2025-08-13 12:49:22 +0600

    configuring deployment

---
+++ localdev/build.gradle.kts
@@ -0,0 +1,16 @@
+import dev.ideascale.localdev.task.KubectlApplyManifest
+import dev.ideascale.localdev.task.KubectlDeleteManifest
+
+plugins {
+    alias(i.plugins.helm.releases)
+    alias(i.plugins.localdev)
+}
+
+helm {
+    releases {
+        create("mcp-client") {
+            from(chart(project = ":app"))
+            installDependsOn(":app:publish")
+        }
+    }
+}
